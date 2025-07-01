23cedb2c429c00eeb062f0e0d76c9453e62cd9a3
natt <natt.korat@cadt.edu.kh>
2025-07-01 10:59:13 +0700

    add few shot

--- utils/prompt.py
+++ utils/prompt.py
@@ -5,6 +5,7 @@ import os
 
 ZERO_SHOT_CLSSION_TEMPLATE = None
 ZERO_SHOT_EVENT_TEMPLATE = None
+FEW_SHOT_EVENT_TEMPLATE = None
 
 prompt_path = os.path.join(os.path.dirname(__file__), 'prompt_templates')
 
@@ -13,6 +14,9 @@ with open(f'{prompt_path}/zero_shot_bin_cls_prompt.txt', 'r', encoding='utf-8') 
 
 with open(f'{prompt_path}/zero_shot_event_prompt.txt', 'r', encoding='utf-8') as file:
     ZERO_SHOT_EVENT_TEMPLATE = file.read()
+
+with open(f'{prompt_path}/few_shot_event_prompt.txt', 'r', encoding='utf-8') as file:
+    FEW_SHOT_EVENT_TEMPLATE = file.read()
 
 def binary_classificaition_prompt_zero_shot(
         article: str,
@@ -78,4 +82,35 @@ def event_extraction_prompt_zero_shot(
     prompt = prompt_template.replace("<article>", article).strip()
     return prompt
 
+def event_extraction_prompt_few_shot(
+        article: str,
+        samples: list,
+        prompt_template: str = FEW_SHOT_EVENT_TEMPLATE
+    ) -> str:
+    """
+    Generates a prompt for extracting events from the given text using a few-shot approach.
 
+    Args:
+        text (str): The text from which to extract events.
+
+    Returns:
+        str: The formatted prompt for event extraction.
+    """
+    samples_str = ""
+    for i, sample in enumerate(samples):
+        samples_str += f"Article {i+1}: {sample['text']}\n"
+        samples_str += f"Events: {sample['events']}\n\n"
+        
+    prompt = prompt_template.replace("<samples>", samples_str)
+    prompt = prompt.replace("<article>", article).strip()
+    return prompt
+
+if __name__ == '__main__':
+    import json
+    with open('structured_events.json', 'r', encoding='utf-8') as file:
+        samples = json.load(file)
+    
+    print(event_extraction_prompt_few_shot(
+        "This is testing article",
+        samples=samples
+    ))
